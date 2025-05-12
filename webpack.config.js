e139c4760ea22685c2c17d3d7c542c664203246f
Alex Guerra <alexander.a.guerra@gmail.com>
2025-05-12 11:02:12 -0500

    Add TypeScript support with initial configuration and entry point

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                use: 'ts-loader'
+            }
+        ]
+    }
+}
